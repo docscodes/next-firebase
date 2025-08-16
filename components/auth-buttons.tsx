e82b9454635216ef9387bb678f4f71be9527bd3b
docscodes <docscodes@gmail.com>
2025-08-16 11:38:00 -0400

    add dropdown menu

--- components/auth-buttons.tsx
+++ components/auth-buttons.tsx
@@ -1,18 +1,74 @@
 "use client";
 
 import { useAuth } from "@/context/auth";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function AuthButtons() {
+  const router = useRouter();
   const auth = useAuth();
 
   return (
     <div>
       {!!auth?.currentUser && (
-        <div className="flex gap-2 items-center">
-          <div>{auth.currentUser.email}</div>
-          <div onClick={auth.logout}>Logout</div>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              {!!auth.currentUser.photoURL && (
+                <Image
+                  src={auth.currentUser.photoURL}
+                  // src="https://github.com/shadcn.png"
+                  alt={`${auth.currentUser.displayName} avatar`}
+                  width={70}
+                  height={70}
+                />
+              )}
+              <AvatarFallback className="text-sky-950">
+                {(auth.currentUser.displayName || auth.currentUser.email)?.[0]}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <div>{auth.currentUser.displayName}</div>
+              <div className="font-normal text-xs">{auth.currentUser.email}</div>
+            </DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+              <Link href="/account">My Account</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+              <Link href="/account/favourites">My Favourites</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+              <Link href="/admin">Admin</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={async () => {
+                await auth.logout();
+                router.refresh();
+              }}
+            >
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
 
       {!auth?.currentUser && (
