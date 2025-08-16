d53b9f0f8b2794bf9212d1bd9bedb43acc38f5d5
docscodes <docscodes@gmail.com>
2025-08-16 10:38:56 -0400

    style the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,27 @@
 import ContinueWithGoogleButton from "@/components/continue-with-google-button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Login() {
-  return <ContinueWithGoogleButton />;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold">Login</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <ContinueWithGoogleButton />
+      </CardContent>
+
+      <CardFooter>
+        Don&apos;t have an account?
+        <Link
+          href="/singup"
+          className="underline pl-2"
+        >
+          Signup here.
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
--- components/continue-with-google-button.tsx
+++ components/continue-with-google-button.tsx
@@ -11,6 +11,7 @@ export default function ContinueWithGoogleButton() {
       onClick={() => {
         auth?.loginWithGoogle();
       }}
+      className="w-full"
     >
       Continue with Google
     </Button>
