c1ab929fb09a8c18d1df9869ed8337a5a2157726
docscodes <docscodes@gmail.com>
2025-08-19 17:06:02 -0400

    query for proterties data

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,13 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
+import { getProperties } from "@/data/properties";
 import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
 
-const Admin = () => {
+const Admin = async () => {
+  const data = await getProperties();
+  console.log(data);
+
   return (
     <div>
       <Breadcrumbs
