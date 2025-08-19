2bbb70e145e8981764328fbc9c1ece1cbe9bb71f
docscodes <docscodes@gmail.com>
2025-08-19 15:45:47 -0400

    createProperty server action

--- app/admin/new/new-property-form.tsx
+++ app/admin/new/new-property-form.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import PropertyDataForm from "@/components/property-form";
+import { useAuth } from "@/context/auth";
 import { propertyDataSchema } from "@/validation/propertySchema";
 import { PlusCircleIcon } from "lucide-react";
 import z from "zod";
+import { createProperty } from "./actions";
 
 const NewPropertyForm = () => {
+  const auth = useAuth();
+
   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
-    console.log(data);
+    const token = await auth?.currentUser?.getIdToken();
+    if (!token) return;
+
+    const response = await createProperty(data, token);
+    console.log(response);
   };
 
   return (
