a2cc3846ab0921c0f513e9ea71ce58c57b7c9c52
docscodes <docscodes@gmail.com>
2025-08-20 17:08:06 -0400

    upload images to firebase storage

--- app/admin/new/new-property-form.tsx
+++ app/admin/new/new-property-form.tsx
@@ -2,26 +2,49 @@
 
 import PropertyDataForm from "@/components/property-form";
 import { useAuth } from "@/context/auth";
-import { propertyDataSchema } from "@/validation/propertySchema";
+import { storage } from "@/firebase/client";
+import { propertySchema } from "@/validation/propertySchema";
+import { ref, uploadBytesResumable, UploadTask } from "firebase/storage";
 import { PlusCircleIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import z from "zod";
+import { savePropertyImages } from "../actions";
 import { createProperty } from "./actions";
 
 const NewPropertyForm = () => {
   const auth = useAuth();
   const router = useRouter();
 
-  const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
+  const handleSubmit = async (data: z.infer<typeof propertySchema>) => {
     const token = await auth?.currentUser?.getIdToken();
     if (!token) return;
 
-    const response = await createProperty(data, token);
-    if (!!response.error) {
+    const { images, ...rest } = data;
+
+    // create property
+    const response = await createProperty(rest, token);
+    if (!!response.error || !response.propertyId) {
       toast.error("Error!", { description: response.error });
       return;
     }
+
+    // upload images
+    const uploadTasks: UploadTask[] = [];
+    const paths: string[] = [];
+
+    images.forEach((image, index) => {
+      if (image.file) {
+        const path = `properties/${response.propertyId}/${Date.now()}-${index}-${image.file.name}`;
+        paths.push(path);
+
+        const storageRef = ref(storage, path);
+        uploadTasks.push(uploadBytesResumable(storageRef, image.file));
+      }
+    });
+
+    await Promise.all(uploadTasks);
+    await savePropertyImages({ propertyId: response.propertyId, images: paths }, token);
 
     toast.success("Success!", { description: "Property created" });
 
