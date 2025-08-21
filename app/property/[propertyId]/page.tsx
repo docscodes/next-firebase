26bcb44bd439a7f7feabfe29bda63eb241792502
docscodes <docscodes@gmail.com>
2025-08-20 23:08:19 -0400

    render the property details

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,4 +1,7 @@
+import PropertyStatusBadge from "@/components/property-status-badge";
 import { getPropertyById } from "@/data/properties";
+import { BathIcon, BedIcon } from "lucide-react";
+import numeral from "numeral";
 import ReactMarkdown from "react-markdown";
 import BackButton from "./back-button";
 
@@ -6,9 +9,15 @@ const Property = async ({ params }: { params: Promise<any> }) => {
   const paramsValue = await params;
   const property = await getPropertyById(paramsValue.propertyId);
 
+  const addressLines = [
+    property.address1,
+    property.address2,
+    property.city,
+    property.postcode,
+  ].filter((addressLine) => !!addressLine);
+
   return (
-    // Right column to be 500px, left to be the remaining
-    <div className="grid grid-cols-[1fr_400px]">
+    <div className="grid grid-cols-[1fr_500px]">
       <div>
         Carousel
         <div className="property-description max-w-screen-md mx-auto py-10 px-4">
@@ -17,7 +26,33 @@ const Property = async ({ params }: { params: Promise<any> }) => {
         </div>
       </div>
 
-      <div className="bg-sky-200 h-screen sticky"></div>
+      <div className="bg-zinc-200 h-screen p-10 sticky top-0 grid place-items-center">
+        <div className="flex flex-col gap-10 w-full">
+          <PropertyStatusBadge
+            status={property.status}
+            className="mr-auto text-base"
+          />
+
+          <h1 className="text-4xl font-semibold">
+            {addressLines.map((addressLine, index) => (
+              <div key={index}>
+                {addressLine}
+                {index < addressLines.length - 1 && ","}
+              </div>
+            ))}
+          </h1>
+
+          <h2 className="text-3xl font-light">£{numeral(property.price).format("0,0")}</h2>
+          <div className="flex gap-10">
+            <div className="flex gap-2">
+              <BedIcon /> {property.bedrooms} Bedrooms
+            </div>
+            <div className="flex gap-2">
+              <BathIcon /> {property.bathrooms} Bathrooms
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
