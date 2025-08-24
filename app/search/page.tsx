5c00e2b13fcf0c0f7f52fee67f539875749cb09d
docscodes <docscodes@gmail.com>
2025-08-24 14:40:07 -0400

    render the filtered property list

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,14 +1,29 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getProperties } from "@/data/properties";
+import imageUrlFormatter from "@/lib/imageUrlFormatter";
+import { BathIcon, BedIcon, HomeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import numeral from "numeral";
 import { Suspense } from "react";
 import FiltersForm from "./filters-form";
 
-const Search = async (searchParams: { searchParams: Promise<any> }) => {
+interface Props {
+  searchParams: Promise<{
+    page?: string;
+    minPrice?: string;
+    maxPrice?: string;
+    minBedrooms?: string;
+  }>;
+}
+
+const Search = async ({ searchParams }: Props) => {
   const searchParamsValues = await searchParams;
-  const parsedPage = parseInt(searchParamsValues?.page);
-  const parsedMinPrice = parseInt(searchParamsValues?.minPrice);
-  const parsedMaxPrice = parseInt(searchParamsValues?.maxPrice);
-  const parsedMinBedrooms = parseInt(searchParamsValues?.minBedrooms);
+  const parsedPage = parseInt(searchParamsValues?.page ?? "");
+  const parsedMinPrice = parseInt(searchParamsValues?.minPrice ?? "");
+  const parsedMaxPrice = parseInt(searchParamsValues?.maxPrice ?? "");
+  const parsedMinBedrooms = parseInt(searchParamsValues?.minBedrooms ?? "");
 
   const page = isNaN(parsedPage) ? 1 : parsedPage;
   const minPrice = isNaN(parsedMinPrice) ? null : parsedMinPrice;
@@ -41,6 +56,60 @@ const Search = async (searchParams: { searchParams: Promise<any> }) => {
           </Suspense>
         </CardContent>
       </Card>
+
+      <div className="grid grid-cols-3 mt-5 gap-5">
+        {data.map((property) => {
+          const addressLines = [
+            property.address1,
+            property.address2,
+            property.city,
+            property.postcode,
+          ]
+            .filter((addressLine) => !!addressLine)
+            .join(", ");
+
+          return (
+            <Card
+              key={property.id}
+              className="overflow-hidden pt-0 pb-0"
+            >
+              <CardContent className="px-0">
+                <div className="h-40 relative bg-sky-50 text-zinc-400 flex flex-col justify-center items-center">
+                  {!!property.images?.[0] && (
+                    <Image
+                      fill
+                      className="object-cover"
+                      src={imageUrlFormatter(property.images[0])}
+                      alt=""
+                    />
+                  )}
+                  {!property.images?.[0] && (
+                    <>
+                      <HomeIcon />
+                      <small>No Image</small>
+                    </>
+                  )}
+                </div>
+                <div className="flex flex-col gap-5 p-5">
+                  <div>{addressLines}</div>
+                  <div className="flex gap-5">
+                    <div className="flex gap-2">
+                      <BedIcon /> {property.bedrooms}
+                    </div>
+                    <div className="flex gap-2">
+                      <BathIcon /> {property.bathrooms}
+                    </div>
+                  </div>
+                  <p className="text-2xl">£{numeral(property.price).format("0,0")}</p>
+                  <Button asChild>
+                    <Link href={`/property/${property.id}`}>View Property</Link>
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
