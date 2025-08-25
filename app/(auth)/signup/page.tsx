2a78984aeeacc2a758bd785284087903ade230ef
docscodes <docscodes@gmail.com>
2025-08-25 17:12:43 -0400

    create the register form schema

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,27 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import RegisterForm from "./register-form";
+
 const Signup = () => {
-  return <div>Signup Page</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold">Register</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <RegisterForm />
+      </CardContent>
+      <CardFooter>
+        Already have an account?
+        <Link
+          href="/login"
+          className="pl-2 underline"
+        >
+          Log in here
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default Signup;
